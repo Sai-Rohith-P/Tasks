86a2e46aff244af4d2c32ef2bacd6094521843d1
Sai-Rohith-P <rohithsai4734@gmail.com>
2024-08-07 22:26:39 +0530

    BootStrap_Project_1

---
+++ Task-6-Bootstrap/javascript1.js
@@ -0,0 +1,14 @@
+const menu1 = document.getElementById("mn1");
+        const close1 = document.getElementById("cl");
+
+        menu1.addEventListener("click", function (event) {
+            menu1.style.display = "none";
+            close1.style.display = "block";
+            event.stopPropagation(); // Prevent the event from bubbling up to the button
+        });
+
+        close1.addEventListener("click", function (event) {
+            close1.style.display = "none";
+            menu1.style.display = "block";
+            event.stopPropagation(); // Prevent the event from bubbling up to the button
+        });
